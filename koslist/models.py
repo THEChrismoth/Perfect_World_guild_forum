b1aa4b5f8e79be60db7ff1aec6359c8e4d485709
THEChrismoth <klevindanila@yandex.ru>
2026-04-12 15:37:55 +0300

    UPDATE: отредактирована модель koslist
    - добавлены шаблоны для отображения страниц
    - добавлены иконки классов

--- koslist/models.py
+++ koslist/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import choices
+from django.templatetags.static import static
 
 class Guild(models.Model):
     name = models.CharField('Название гильдии', max_length = 100)
@@ -45,6 +46,31 @@ class Player(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_icon_url(self):
+        """Возвращаем URL иконки для класса персонажа"""
+        icons = {
+            'var': 'class_icon/voin.png',
+            'mag': 'class_icon/mag.png',
+            'luk': 'class_icon/luk.png',
+            'zhrec': 'class_icon/zhrec.png',
+            'obor': 'class_icon/obor.png',
+            'dru': 'class_icon/dru.png',
+            'sin': 'class_icon/sin.png',
+            'sham': 'class_icon/sham.png',
+            'mist': 'class_icon/mist.png',
+            'strazh': 'class_icon/strazh.png',
+            'pal': 'class_icon/pal.png',
+            'strel': 'class_icon/strel.png',
+            'priz': 'class_icon/priz.png',
+            'zhnec': 'class_icon/zhnec.png',
+            'makaka': 'class_icon/makaka.png',
+            'bard': 'class_icon/bard1.png',
+        }
+        icon_path = icons.get(self.player_class, '')
+        if icon_path:
+            return static(icon_path)
+        return ''
 
 
 
--- koslist/views.py
+++ koslist/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Guild, Player
 
-# Create your views here.
+def kos_view(request):
+    guilds = Guild.objects.all()
+    players = Player.objects.all()
+
+    players_by_class = {}
+    for player in players:
+        if player.player_class not in players_by_class:
+            players_by_class[player.player_class] = []
+        players_by_class[player.player_class].append(player)
+
+    context = {
+            'players_by_class': players_by_class,
+            'guilds': guilds,
+           }
+    
+    return render(request, 'koslist/koslist.html', context)
